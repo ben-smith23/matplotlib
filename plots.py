536775cfeffc4113ae45eb20c883725c46e8e93c
ben-smith23 <112433581+ben-smith23@users.noreply.github.com>
2022-10-24 19:34:14 -0700

    Update plots.py

--- plots.py
+++ plots.py
@@ -12,8 +12,14 @@ periods = {"0-0.5":0,"0.5-1":0,"1-1.5":0,"1.5-2":0,"2-2.5":0,"2.5-3":0,"3-3.5":0
 oribital_period = asteroids[0]['period_yr']
 op = float(oribital_period)
 
-for period in periods:
-    for asteroid in asteroids:
+for asteroid in asteroids:
+    for value in periods:
         if op >= 0 and op <= 0.5:
-            periods += 1
+            value += 1
 print(periods)
+
+'''
+fig, ax = plt.subplots()
+ax.bar(test2, test1)
+plt.show()
+'''
