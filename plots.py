ed91a2c50464ab6b8daceba2a38a1c4a48f3883c
ben-smith23 <112433581+ben-smith23@users.noreply.github.com>
2022-10-25 19:32:57 -0700

    Update plots.py

--- plots.py
+++ plots.py
@@ -59,10 +59,12 @@ df = pd.read_csv(r'adultarrests.csv', encoding="latin-1")
 
 drug = list(df.groupby('Year')['Drug Felony'].sum())
 violent = list(df.groupby('Year')['Violent Felony'].sum())
-years = [1970,1971,1972,1973,1974,1975,1976,1978,1978,1979,1980,1981,1982,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021]
+years = [1970,1971,1972,1973,1974,1975,1976,1977,1978,1979,1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021]
 
-df.plot(years, drug)
+plt.plot(years, drug)
+plt.plot(years, violent)
 plt.xlabel('Year')
-plt.ylabel('Number Felonies')
-plt.legend(['Drug', 'Violnet'])
+ax.set_xlim([1970, 2021])
+plt.ylabel('Number of Felonies')
+plt.legend(['Drug', 'Violent'])
 plt.show()
